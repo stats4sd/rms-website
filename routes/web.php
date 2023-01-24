32e984ad50860d8d42443ab298be623172299a06
Dave Mills <d.e.mills@stats4sd.org>
2023-01-24 09:19:50 +0000

    WIP - get calendar events from MS

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Dcblogdev\MsGraph\Facades\MsGraph;
+use Dcblogdev\MsGraph\Facades\MsGraphAdmin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,79 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('login', function () {
+
+    $tenant = env('MSGRAPH_TENANT_ID');
+//    $url = "https://login.microsoftonline.com/{$tenant}/oauth2/v2.0/token";
+
+//    // application level login
+//    $response = \Illuminate\Support\Facades\Http::asForm()->post($url, [
+//        'client_id' => env('MSGRAPH_CLIENT_ID'),
+//        'client_secret' => env('MSGRAPH_SECRET_ID'),
+//        'scope' => 'https://graph.microsoft.com/.default',
+//        'grant_type' => 'client_credentials',
+//        ])->json();
+//
+//    cache()->set('azure_token', $response['access_token']);
+
+    // return redirect('test');
+
+    // User Login
+    $url = "https://login.microsoftonline.com/{$tenant}/oauth2/v2.0/authorize";
+    $client_id = env('MSGRAPH_CLIENT_ID');
+
+    return redirect(
+        "{$url}?client_id={$client_id}&scope="
+        . urlencode('https://graph.microsoft.com/.default') .
+        "&response_type=code&redirect_uri="
+        . urlencode('https://rms.test/after_login') .
+        "&response_mode=query"
+    );
+
+
+});
+
+Route::get('after_login', function () {
+
+    $tenant = env('MSGRAPH_TENANT_ID');
+
+    $url = "https://login.microsoftonline.com/{$tenant}/oauth2/v2.0/token";
+
+    $response = \Illuminate\Support\Facades\Http::asForm()->post($url, [
+        'client_id' => env('MSGRAPH_CLIENT_ID'),
+        'client_secret' => env('MSGRAPH_SECRET_ID'),
+        'scope' => 'https://graph.microsoft.com/.default',
+        'code' => request()->query('code'),
+        'grant_type' => 'authorization_code',
+        'redirect_uri' => url('after_login'),
+    ])->json();
+
+    cache()->set('azure_token', $response['access_token']);
+
+    return redirect('test');
+});
+
+Route::get('test', function () {
+
+    $token = cache()->get('azure_token');
+
+    $response = \Illuminate\Support\Facades\Http::withToken($token)
+        ->get(env('MSGRAPH_ENDPOINT') . '/groups')->json();
+
+
+    $group = collect($response['value'])->filter(fn($item) => $item['mail'] === 'caltest@stats4sd.org');
+
+    $rmsId = $group->first()['id'];
+
+    $calendar = \Illuminate\Support\Facades\Http::withToken($token)
+        ->get(env('MSGRAPH_ENDPOINT') . '/groups/' . $rmsId . '/calendar')
+        ->json();
+
+    $events = \Illuminate\Support\Facades\Http::withToken($token)
+        ->get(env('MSGRAPH_ENDPOINT') . '/groups/' . $rmsId . '/calendar/events')
+    ->
+    json();
+    ddd($events['value']);
 });
