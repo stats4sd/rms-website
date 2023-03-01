fb65996b5a7d54d945d7487e9d274dc1fdc12f67
Dave Mills <d.e.mills@stats4sd.org>
2023-03-01 10:43:27 +0000

    enable formattting for event description

--- resources/views/components/event-display.blade.php
+++ resources/views/components/event-display.blade.php
@@ -2,14 +2,14 @@
     <div tabindex="0" class="collapse collapse-arrow ">
 
         <h4 class="collapse-title card-title grid grid-row md:grid-cols-4 font-light">
-            
+
            <div class="font-semibold md:col-span-3"> {{ $event->title }}</div>
            <div class="pl-4 md:col-span-1">{{ ($event->start)->format('d/m/Y G:i')}}</div>
 
         </h4>
         <div class="collapse-content pl-5">
             <p class="pb-4">
-                {{ $event->body }}
+                {!! $event->body !!}
             </p>
 
 
