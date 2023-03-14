927e585c5373ffd54a34a81c55881cdffd665e05
Dave Mills <d.e.mills@stats4sd.org>
2023-03-14 17:57:55 +0000

    fix event accordion: - previously, links inside the event details were not clickable. This fixes that by swapping the daisyUI collapse for an accordion implimented in AplineJS

--- resources/views/components/event-display.blade.php
+++ resources/views/components/event-display.blade.php
@@ -1,35 +1,59 @@
-<div class="card bg-base-100 shadow-md rounded-none event">
-    <div tabindex="0" class="collapse collapse-arrow ">
+<div
+    x-data="{
+        id: {{$event->id}},
+        get open() {
+            return this.active === this.id
+        },
+        set open(value) {
+            this.active = value ? this.id : null
+        }
+    }"
+    class="card bg-base-100 shadow-md rounded-none event"
+>
 
-        <h4 class="collapse-title card-title grid grid-row md:grid-cols-4 font-light">
-            
-           <div class="font-semibold md:col-span-3"> {{ $event->title }}</div>
-           <div class="pl-4 md:col-span-1">{{ ($event->start)->format('d/m/Y G:i')}}</div>
-
-        </h4>
-        <div class="collapse-content pl-5">
-            <p class="pb-4">
-                {!! $event->body !!}
-            </p>
-            <div class="card">
-                <div class="card-body pl-0">
-                    <h4>Related Resources</h4>
-                    <ul class="pb-4 pl-4 list-disc">
-
-                        @foreach($event->eventResources as $resource)
-                            <li><a href="{{ $resource->url }}">{{ $resource->title }}</a></li>
-                        @endforeach
-                    </ul>
-
-                </div>
+    <button class="text-left">
+        <h4
+            x-on:click="open = !open"
+            :aria-expanded="open"
+            class="card-title grid grid-row md:grid-cols-10 font-light p-3"
+        >
+            <div class="font-semibold md:col-span-6"> {{ $event->title }}</div>
+            <div class="pl-4 md:col-span-3">
+                {{ ($event->start)->format('d/m/Y G:i')}}
             </div>
+            <div class="md:col-span-1">
+                <span x-show="open" aria-hidden="true" class="ml-4">&minus;</span>
+                <span x-show="!open" aria-hidden="true" class="ml-4">&plus;</span>
+            </div>
+        </h4>
+    </button>
+    <div
+        x-show="open"
+        x-collapse
+        class="pl-5 pb-5"
+    >
+        <p class="pb-4">
+            {!! $event->body !!}
+        </p>
+        <div class="card">
+            <div class="card-body pl-0">
+                <h4>Related Resources</h4>
+                <ul class="pb-4 pl-4 list-disc">
 
-            @if(!$isPast)
-                <p class="pb-4">
-                    {{ ($event->joining_instructions ?? $event->eventType?->joining_instructions) ?? '' }}
-                </p>
-                <a href="{{ ($event->registration_url ?? $event->eventType?->registration_url) ?? '' }}">Click here to register</a>
-            @endif
+                    @foreach($event->eventResources as $resource)
+                        <li><a href="{{ $resource->url }}">{{ $resource->title }}</a></li>
+                    @endforeach
+                </ul>
+
+            </div>
         </div>
+
+        @if(!$isPast)
+            <p class="pb-4">
+                {{ ($event->joining_instructions ?? $event->eventType?->joining_instructions) ?? '' }}
+            </p>
+            <a href="{{ ($event->registration_url ?? $event->eventType?->registration_url) ?? '' }}">Click here to
+                register</a>
+        @endif
     </div>
 </div>
